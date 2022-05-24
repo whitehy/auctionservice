4650f9bf2823d5163de7f0f7f167c96e8b094975
Hayeun Lee <whitehy@gmail.com>
2022-05-23 20:36:23 -0700

    32. Creating a Common Middleware

--- src/handlers/createAuction.js
+++ src/handlers/createAuction.js
@@ -1,10 +1,7 @@
 import { v4 as uuid } from "uuid";
 import AWS from "aws-sdk";
-import middy from "@middy/core";
-import httpJsonBodyParser from "@middy/http-json-body-parser";
-import httpEventNormalizer from "@middy/http-event-normalizer";
-import httpErrorHandler from "@middy/http-error-handler";
-import createError from "http-errors"
+import commonMiddleware from "../lib/commonMiddleware";
+import createError from "http-errors";
 
 const dynamodb = new AWS.DynamoDB.DocumentClient();
 
@@ -19,17 +16,17 @@ async function createAuction(event, context) {
     createdAt: now.toISOString(),
   };
 
-try{
-  await dynamodb
-  .put({
-    TableName: process.env.AUCTIONS_TABLE_NAME,
-    Item: auction,
-  })
-  .promise();
-} catch(error){
-  console.error(error);
-  throw new createError.InternalServerError(error);
-}
+  try {
+    await dynamodb
+      .put({
+        TableName: process.env.AUCTIONS_TABLE_NAME,
+        Item: auction,
+      })
+      .promise();
+  } catch (error) {
+    console.error(error);
+    throw new createError.InternalServerError(error);
+  }
 
   return {
     statusCode: 201,
@@ -37,7 +34,4 @@ try{
   };
 }
 
-export const handler = middy(createAuction)
-  .use(httpJsonBodyParser())
-  .use(httpEventNormalizer())
-  .use(httpErrorHandler())
+export const handler = commonMiddleware(createAuction);
--- src/handlers/getAuctions.js
+++ src/handlers/getAuctions.js
@@ -1,8 +1,5 @@
 import AWS from "aws-sdk";
-import middy from "@middy/core";
-import httpJsonBodyParser from "@middy/http-json-body-parser";
-import httpEventNormalizer from "@middy/http-event-normalizer";
-import httpErrorHandler from "@middy/http-error-handler";
+import commonMiddleware from "../lib/commonMiddleware";
 import createError from "http-errors";
 
 const dynamodb = new AWS.DynamoDB.DocumentClient();
@@ -11,11 +8,13 @@ async function getAuctions(event, context) {
   let auctions;
 
   try {
-    const result = await dynamodb.scan({
-      TableName: process.env.AUCTIONS_TABLE_NAME,
-    }).promise();
+    const result = await dynamodb
+      .scan({
+        TableName: process.env.AUCTIONS_TABLE_NAME,
+      })
+      .promise();
 
-    auctions = result.Items
+    auctions = result.Items;
   } catch (error) {
     console.error(error);
     throw new createError.InternalServerError(error);
@@ -27,7 +26,4 @@ async function getAuctions(event, context) {
   };
 }
 
-export const handler = middy(getAuctions)
-  .use(httpJsonBodyParser())
-  .use(httpEventNormalizer())
-  .use(httpErrorHandler());
+export const handler = commonMiddleware(getAuctions);
