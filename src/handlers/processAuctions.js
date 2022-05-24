34639b3fcb5c5372d9f8a44a7c9e0047201d7d5c
Hayeun Lee <whitehy@gmail.com>
2022-05-24 13:58:04 -0700

    37. Identifying Ended Auctions

--- src/handlers/processAuctions.js
+++ src/handlers/processAuctions.js
@@ -1,4 +1,7 @@
+import {getEndedAuctions} from "../lib/getEndedAuctions"
+
 async function processAuctions(event, context){
-    console.log('processing auctions!')
+    const auctionsToClose = await getEndedAuctions();
+    console.log(auctionsToClose);
 }
 export const handler = processAuctions;
