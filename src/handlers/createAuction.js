d14dac8f4e5eb4fb31e6a589ae0c4743961f36f0
Hayeun Lee <whitehy@gmail.com>
2022-05-24 17:35:38 -0700

    44. (Challenge) Create Auction Schema Validation

--- src/handlers/createAuction.js
+++ src/handlers/createAuction.js
@@ -1,7 +1,9 @@
-import { v4 as uuid } from "uuid";
-import AWS from "aws-sdk";
-import commonMiddleware from "../lib/commonMiddleware";
-import createError from "http-errors";
+import { v4 as uuid } from 'uuid';
+import AWS from 'aws-sdk';
+import createError from 'http-errors';
+import validator from '@middy/validator';
+import commonMiddleware from '../lib/commonMiddleware';
+import createAuctionSchema from '../lib/schemas/createAuctionSchema';
 
 const dynamodb = new AWS.DynamoDB.DocumentClient();
 
@@ -9,27 +11,25 @@ async function createAuction(event, context) {
   const { title } = event.body;
   const now = new Date();
   const endDate = new Date();
-  endDate.setHours(now.getHours() +1)
+  endDate.setHours(now.getHours() + 1);
 
   const auction = {
     id: uuid(),
     title,
-    status: "OPEN",
+    status: 'OPEN',
     createdAt: now.toISOString(),
     endingAt: endDate.toISOString(),
     highestBid: {
-      amount: 0
-    }
+      amount: 0,
+    },
   };
 
   try {
-    await dynamodb
-      .put({
-        TableName: process.env.AUCTIONS_TABLE_NAME,
-        Item: auction,
-      })
-      .promise();
-  } catch (error) {
+    await dynamodb.put({
+      TableName: process.env.AUCTIONS_TABLE_NAME,
+      Item: auction,
+    }).promise();
+  } catch(error) {
     console.error(error);
     throw new createError.InternalServerError(error);
   }
@@ -40,4 +40,5 @@ async function createAuction(event, context) {
   };
 }
 
-export const handler = commonMiddleware(createAuction);
+export const handler = commonMiddleware(createAuction)
+  .use(validator({ inputSchema: createAuctionSchema }));
