d79551708da15fb5116201208185807df89f4a9e
Hayeun Lee <whitehy@gmail.com>
2022-05-24 17:22:32 -0700

    42. JSON Schema Validation - Get Auctions Status

--- src/handlers/getAuctions.js
+++ src/handlers/getAuctions.js
@@ -1,23 +1,25 @@
 import AWS from "aws-sdk";
 import commonMiddleware from "../lib/commonMiddleware";
 import createError from "http-errors";
+import validator from "@middy/validator";
+import getAuctionsSchema from "../lib/schemas/getAuctionsSchema";
 
 const dynamodb = new AWS.DynamoDB.DocumentClient();
 
 async function getAuctions(event, context) {
   const { status } = event.queryStringParameters;
   let auctions;
-  
+
   const params = {
     TableName: process.env.AUCTIONS_TABLE_NAME,
-    IndexName: 'statusAndEndDate',
-    KeyConditionExpression: '#status = :status',
+    IndexName: "statusAndEndDate",
+    KeyConditionExpression: "#status = :status",
     ExpressionAttributeValues: {
-      ':status': status,
+      ":status": status,
     },
     ExpressionAttributeNames: {
-      '#status': 'status',
-    }
+      "#status": "status",
+    },
   };
 
   try {
@@ -35,4 +37,6 @@ async function getAuctions(event, context) {
   };
 }
 
-export const handler = commonMiddleware(getAuctions);
+export const handler = commonMiddleware(getAuctions).use(
+  validator({ inputSchema: getAuctionsSchema, useDefaults: true })
+);
